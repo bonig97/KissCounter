a8abd689bb3637834028145b59e7887057751ac8
Gabriele Boni <bonig97@gmail.com>
2023-05-21 23:07:01 +0200

    Init read permission request before reading from external memory

--- app/src/main/java/com/example/kisscounter/MainActivity.java
+++ app/src/main/java/com/example/kisscounter/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.kisscounter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -19,6 +23,11 @@ import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static final String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +37,18 @@ public class MainActivity extends AppCompatActivity {
         try {
             String downloadsPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
             String filePath = downloadsPath + File.separator + "J.txt";
+
+            // Check if we have write permission
+            int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // We don't have permission so prompt the user
+                ActivityCompat.requestPermissions(
+                        this,
+                        PERMISSIONS_STORAGE,
+                        REQUEST_EXTERNAL_STORAGE
+                );
+            }
 
             File file = new File(filePath);
             FileInputStream fis = new FileInputStream(file);
